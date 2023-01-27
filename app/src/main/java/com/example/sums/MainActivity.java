3fe5ff3b3863ace0ec2c4eaa493674486e741279
SrPampa <98409736+SrPampa@users.noreply.github.com>
2023-01-27 12:32:32 +0100

    empecemos bien

---
+++ app/src/main/java/com/example/sums/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.sums;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.iconomates);
+
+    }
+
+
+}
